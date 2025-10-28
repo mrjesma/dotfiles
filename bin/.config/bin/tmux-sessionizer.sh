3012d996732a9bd0df85e3296caec2fadb135d9d
mrjesma <airton.orlandini@hotmail.com>
2025-10-28 17:29:37 -0300

    Update tmux scripts

--- bin/.config/bin/tmux-sessionizer.sh
+++ bin/.config/bin/tmux-sessionizer.sh
@@ -1,43 +1,29 @@
 #!/usr/bin/env bash
 # This script selects a host from a fzf list and ssh to it in a new (or switch to an existing) tmux session.
-#
 # Stole and modified from: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/bin/tmux-sessionizer
 
-if [[ $# -eq 1 ]]; then
-  selected_name=$(echo ${1^^} | cut -d '@' -f2)
-  selected=$1
-  host=$1
-else
-  selected=$(cat ~/repos/siemens-env/hosts | fzf --no-sort --cycle --print-query | tail -1)
-  echo $selected
-  selected_name=$(echo $selected | awk -F'|' '{print $1}' | sed 's/ $//' | sed 's/ /_/g')
-  host=$(echo $selected | awk -F'|' '{print $2}')
+IFS=',' read -r description host command <<<"$(cat ~/repos/siemens-env/hosts | fzf --no-sort --cycle --print-query --delimiter ',' --with-nth=1,2 | tail -1)"
 
-  # if no entry was selected in fzf get the search string and uses it as selected_host and host
-  selected_name=${selected_name:=$selected}
-  host=${host:=$selected}
-fi
-
-if [[ -z $selected ]]; then
-  exit 0
-fi
+[[ -z $description || -z $host ]] && exit 0
+[[ -z $command ]] && command="ssh $host"
+description="$(echo $description | xargs | tr '[:lower:]' '[:upper:]' | tr ' ' '_')"
 
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  # No tmux server started. Create new session and attach it to it.
-  tmux new-session -s "${selected_name^^}" "ssh $host"
-  exit 0
+	# No tmux server started. Create new session and attach it to it.
+	tmux new-session -s "${description}" "${command}"
+	exit 0
 fi
 
-if ! tmux has-session -t="${selected_name^^}" 2>/dev/null; then
-  # Tmux server is started but no session with name $selected_name. Create new dettached session"
-  tmux new-session -ds "${selected_name^^}" "ssh $host"
+if ! tmux has-session -t="${description}" 2>/dev/null; then
+	# Tmux server is started but no session with name $description. Create new dettached session"
+	tmux new-session -ds "${description}" "${command}"
 fi
 
-# Switch or attach to existing tmux session with name: $selected_name
+# Switch or attach to existing tmux session with name: $description
 if [[ -z $TMUX ]]; then
-  tmux attach-session -t "${selected_name^^}"
+	tmux attach-session -t "${description}"
 else
-  tmux switch-client -t "${selected_name^^}"
+	tmux switch-client -t "${description}"
 fi
