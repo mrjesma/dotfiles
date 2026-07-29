76cf66662edfa0fa261bc3114ff91e806e0fab13
mrjesma <airton.orlandini@hotmail.com>
2026-07-29 10:59:28 -0300

    Add capability to run external commands

--- bin/.config/bin/tmux-sessionizer.sh
+++ bin/.config/bin/tmux-sessionizer.sh
@@ -2,28 +2,34 @@
 # This script selects a host from a fzf list and ssh to it in a new (or switch to an existing) tmux session.
 # Stole and modified from: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/bin/tmux-sessionizer
 
-IFS=',' read -r description host command <<<"$(cat ~/repos/siemens-env/hosts | fzf --no-sort --cycle --print-query --delimiter ',' --with-nth=1,2 | tail -1)"
+IFS=',' read -r DESCRIPTION HOST COMMAND <<<"$(fzf --ansi --no-sort --cycle --print-query --delimiter ',' --with-nth '{1} - {2}' < ~/repos/siemens-env/hosts | tail -1)"
 
-[[ -z $description || -z $host ]] && exit 0
-[[ -z $command ]] && command="ssh $host"
-description="$(echo $description | xargs | tr '[:lower:]' '[:upper:]' | tr ' ' '_')"
+[[ -z $DESCRIPTION || -z $HOST ]] && exit 0
+[[ -z $COMMAND ]] && COMMAND="ssh $HOST"
+
+if [[ $COMMAND == *"&" ]]; then
+	eval "$COMMAND"
+	exit 0
+fi
+
+DESCRIPTION="$(echo $DESCRIPTION | sed 's/\x1b\[[0-9;]*m//g' | xargs | tr '[:lower:]' '[:upper:]' | tr ' ' '_')"
 
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
 	# No tmux server started. Create new session and attach it to it.
-	tmux new-session -s "${description}" "${command}"
+	tmux new-session -s "${DESCRIPTION}" "${COMMAND}"
 	exit 0
 fi
 
-if ! tmux has-session -t="${description}" 2>/dev/null; then
-	# Tmux server is started but no session with name $description. Create new dettached session"
-	tmux new-session -ds "${description}" "${command}"
+if ! tmux has-session -t="${DESCRIPTION}" 2>/dev/null; then
+	# Tmux server is started but no session with name $DESCRIPTION. Create new dettached session"
+	tmux new-session -ds "${DESCRIPTION}" "${COMMAND}"
 fi
 
-# Switch or attach to existing tmux session with name: $description
+# Switch or attach to existing tmux session with name: $DESCRIPTION
 if [[ -z $TMUX ]]; then
-	tmux attach-session -t "${description}"
+	tmux attach-session -t "${DESCRIPTION}"
 else
-	tmux switch-client -t "${description}"
+	tmux switch-client -t "${DESCRIPTION}"
 fi
